eb6ef7e0a9e2457ebcfbd9d596e8f3f8db8cf0e8
HansenChristoffer <chris.hansen.ch@outlook.com>
2021-03-16 13:29:32 +0100

    Added another "any" check. When debugLevel is set to 0 then ALL debugMsg will run

--- SimplisticLog.js
+++ SimplisticLog.js
@@ -6,7 +6,7 @@ export function debugMsg(msg = DEFAULT_MESSAGE, level = 0, asObject = true) {
   const TIMESTAMP = new Date().toLocaleString().replace(",", "");
   const SCOPE = new Error().stack.split("\n")[2].trim().split(" ")[1];
 
-  if (level === debugLevel || level === 0) {
+  if (level === debugLevel || level === 0 || debugLevel === 0) {
     if (!asObject) {
       console.log(`<${TIMESTAMP}> [Level=${level}, Scope=${SCOPE}] -> ${msg}`);
     } else if (asObject) {
